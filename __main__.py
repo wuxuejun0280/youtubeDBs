eaf3d3544ce2c652ecea46fc8212fbf3de185bc2
wuxuejun0280 <996977662@qq.com>
2019-10-29 17:00:30 +0200

    start collect at 18:00:00 today for every 1 hour

--- __main__.py
+++ __main__.py
@@ -1,12 +1,19 @@
 import pexpect
 import sys
 import sched, time
-s = sched.scheduler(time.time, time.sleep)
+from datetime import datetime
+from threading import Timer
 
+s = sched.scheduler(time.time, time.sleep)
+x=datetime.today()
+y=x.replace(day=x.day, hour=18, minute=0, second=0, microsecond=0)
+delta_t=y-x
+secs = delta_t.seconds+1
 
 def collect(count = 0):
+    print(datetime.now())
     if count < 24:
-        s.enter(120, 1, collect, argument=(count+1,))
+        s.enter(3600, 1, collect, argument=(count+1,))
 
     child = pexpect.spawn('python2.7 /home/wuxuejun0820/pytomo/start_crawl.py https://www.youtube.com/watch?v=J5pwZCcC8cg&t=22s')
     child.logfile = sys.stdout
@@ -17,5 +24,6 @@ def collect(count = 0):
     child.send('\n')
 
 
-s.enter(120, 1, collect, argument=(0,))
+s.enter(secs, 1, collect, argument=(0,))
 s.run()
+
