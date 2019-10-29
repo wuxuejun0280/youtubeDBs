ea0f50fda526c5a360adc9daf97e5d78f4b3fc5f
wuxuejun0280 <996977662@qq.com>
2019-10-29 16:21:41 +0200

    add python file to auto collect data

---
+++ __main__.py
@@ -0,0 +1,21 @@
+import pexpect
+import sys
+import sched, time
+s = sched.scheduler(time.time, time.sleep)
+
+
+def collect(count = 0):
+    if count < 24:
+        s.enter(3600, 1, collect, argument=(count+1,))
+
+    child = pexpect.spawn('python2.7 /home/wuxuejun0820/pytomo/start_crawl.py https://www.youtube.com/watch?v=J5pwZCcC8cg&t=22s')
+    child.logfile = sys.stdout
+
+    child.expect('Are you ok to start crawling?', timeout=9999)
+    child.send('Y\n')
+    child.expect('Press Enter to exit', timeout=9999999999)
+    child.send('\n')
+
+
+s.enter(3600, 1, collect, argument=(0,))
+s.run()
