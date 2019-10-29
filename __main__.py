913cf1a30574583a073453ac38c3dffe661ac022
wuxuejun0280 <996977662@qq.com>
2019-10-29 16:27:11 +0200

    change repeat period to 2 min

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ s = sched.scheduler(time.time, time.sleep)
 
 def collect(count = 0):
     if count < 24:
-        s.enter(3600, 1, collect, argument=(count+1,))
+        s.enter(120, 1, collect, argument=(count+1,))
 
     child = pexpect.spawn('python2.7 /home/wuxuejun0820/pytomo/start_crawl.py https://www.youtube.com/watch?v=J5pwZCcC8cg&t=22s')
     child.logfile = sys.stdout
@@ -17,5 +17,5 @@ def collect(count = 0):
     child.send('\n')
 
 
-s.enter(3600, 1, collect, argument=(0,))
+s.enter(120, 1, collect, argument=(0,))
 s.run()
